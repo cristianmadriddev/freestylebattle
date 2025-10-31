c2f15c4fa134e94d1d4c691d409c556f20b59b52
cristianmadriddev <cristianmadriddev@users.noreply.github.com>
2025-10-31 18:09:59 +0000

    deploy: 1f6ae314cd68d639940e47b0a3fb2b76f0256ae9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1439272671"
+    serviceWorkerVersion: "889977606"
   }
 });
