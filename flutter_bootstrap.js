e4724818b49db73cede53747620ef3a2faa8c4c8
cristianmadriddev <cristianmadriddev@users.noreply.github.com>
2025-10-31 17:55:12 +0000

    deploy: 98550d72a03a2b4d0cdb7a3f31424ae8ef881132

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3840643767"
+    serviceWorkerVersion: "1571443072"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,9 +33,9 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "3f650630e97f4076d5e550968818bc14",
+"flutter_bootstrap.js": "af6589f66286779c643fa032a387650f",
 "version.json": "8bbe7429bbe84b62bf4bc4813dfc9753",
-"main.dart.js": "2f425b00fe7b95a5a2c2c666c34c53c2"};
+"main.dart.js": "616825ca48a3fbe60b4b7d62fe4ed057"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
